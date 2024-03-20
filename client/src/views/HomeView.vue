46927860e5ed17be1e5dfdbd8acfa90d3db03947
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-03-20 12:00:45 +0300

    Add dependencies and routes for video downloading and progress tracking

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,22 +1,32 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import axios from "axios";
-
 const link = ref("");
+const formaVid = ref("");
 const errorMessage = ref(""); // Variable to store error message
+const Formats = ref("");
+const downloadProgress = ref(null); // Variable to store download progress
+
+const progress = ref(0); // Variable
 
 const submitForm = () => {
   // Reset error message
   errorMessage.value = "";
+  Formats.value = "";
 
   // Send request to server
   axios
-    .post("http://localhost:3000/getData", { link: link.value })
+    .post("http://localhost:3000/getData", {
+      link: link.value,
+      format: formaVid.value,
+    })
     .then((response) => {
       // Handle successful response
       console.log(response.data);
       errorMessage.value = response.data.message; // Display message from the server
+      Formats.value = response.data.formats; // Display message from the server
     })
+
     .catch((error) => {
       // Handle error
       console.error("Error:", error);
@@ -32,198 +42,185 @@ const submitForm = () => {
       }
     });
 };
+// Function to establish SSE connection and receive progress updates
+const initSSE = () => {
+  const eventSource = new EventSource("http://localhost:3000/progress");
+  eventSource.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    progress.value = data.progress; // Update download progress
+  };
+};
+
+// Initialize SSE connection when the component is mounted
+onMounted(() => {
+  initSSE();
+});
 </script>
+
 <template>
   <section class="hero">
     <div class="container">
-      <div class="row">
-        <div class="col">
-          <img
-            loading="lazy"
-            src="/assets/youtube-save-logo.png"
-            alt="Video Max"
-            width="180"
-            height="52"
-            class="img-fluid"
-          />
-        </div>
-        <!-- <div class="col">
-            <img
-              loading="lazy"
-              src="/assets/img/ps.png"
-              alt="Video Max"
-              width="20%"
-              class="img-fluid"
+      <div class="frm_dl dl_cm_st">
+        <form
+          method="post"
+          action=""
+          @submit.prevent="submitForm"
+          onsubmit="return download();"
+        >
+          <div class="input-group">
+            <span class="input-group-text"
+              ><svg
+                width="28"
+                height="28"
+                viewBox="0 0 28 28"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m19.775 14.825 3.3-3.3a4.666 4.666 0 1 0-6.6-6.6l-3.3 3.3M8.225 13.175l-3.299 3.3a4.666 4.666 0 0 0 0 6.6v0a4.666 4.666 0 0 0 6.6 0l3.3-3.3M6.58 9.87l-2.474-.817M9.87 6.58l-.817-2.473M21.42 18.13l2.484.817M18.129 21.42l.817 2.485M10.698 17.302l6.604-6.604"
+                  stroke="#303030"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+              ></span
+            >
+
+            <input
+              type="text"
+              onpaste="setTimeout(() => {download()}, 0);"
+              class="form-control"
+              name="link"
+              v-model="link"
+              required
+              placeholder="Paste your video link here"
             />
-          </div> -->
-      </div>
-      <div class="hero_body">
-        <!-- <img
-            loading="lazy"
-            src="/assets/youtube-save-logo.png"
-            alt="Video Max"
-            width="180"
-            height="52"
-            class="img-fluid"
-          /> -->
-        <div class="text-center"><span class="line_head"></span></div>
-        <span class="sub_tit">Online Video</span>
-        <h1>Youtube Downloader</h1>
-        <div class="txt_hero">
-          <div id="txt_hero" class="carousel slide" data-bs-ride="true">
-            <div class="carousel-indicators">
-              <button
-                type="button"
-                data-bs-target="#txt_hero"
-                data-bs-slide-to="0"
-                class="active"
-                aria-current="true"
-                aria-label="Slide 1"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#txt_hero"
-                data-bs-slide-to="1"
-                aria-label="Slide 2"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#txt_hero"
-                data-bs-slide-to="2"
-                aria-label="Slide 3"
-              ></button>
-            </div>
-            <div class="carousel-inner pb-4">
-              <div class="carousel-item active">
-                <p>
-                  Do not look more! Explore our
-                  <span>ٰYoutubeSave</span> video downloader, a free solution to
-                  quickly download videos or music with just one click!
-                </p>
-              </div>
-              <div class="carousel-item">
-                <p>
-                  Download your favorite Youtbe videos with
-                  <span>ٰYoutubeSave</span> The ultimate YouTube downloader tool
-                  that transforms online videos into your personal device.
-                </p>
-              </div>
-              <div class="carousel-item">
-                <p>
-                  Improve your YouTube experience with our
-                  <span>User friendly downloader tool</span> put your favourite
-                  videos at your fingertips for offline watching.
-                </p>
-              </div>
-            </div>
+            <button
+              type="submit"
+              submit="true"
+              class="btn_dl d-none d-md-block"
+            >
+              <span>Download <i class="fa-solid fa-arrow-right"></i></span>
+              <span
+                class="d-none spinner-border spinner-border-sm"
+                aria-hidden="true"
+              ></span>
+            </button>
+            <button
+              aria-label="Download"
+              type="submit"
+              submit="true"
+              class="btn_dl p-3 d-block d-md-none"
+              style="border-radius: 20px 45px 45px 20px !important"
+            >
+              <svg
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M19 12.076H5M14 17.076l5-5M14 7.076l5 5"
+                  stroke="#fff"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+              <span
+                class="d-none spinner-border spinner-border-sm"
+                aria-hidden="true"
+              ></span>
+            </button>
           </div>
-        </div>
-        <div class="frm_dl dl_cm_st">
-          <form method="post" @submit.prevent="submitForm">
-            <div class="input-group">
-              <span class="input-group-text"
-                ><svg
-                  width="28"
-                  height="28"
-                  viewBox="0 0 28 28"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="m19.775 14.825 3.3-3.3a4.666 4.666 0 1 0-6.6-6.6l-3.3 3.3M8.225 13.175l-3.299 3.3a4.666 4.666 0 0 0 0 6.6v0a4.666 4.666 0 0 0 6.6 0l3.3-3.3M6.58 9.87l-2.474-.817M9.87 6.58l-.817-2.473M21.42 18.13l2.484.817M18.129 21.42l.817 2.485M10.698 17.302l6.604-6.604"
-                    stroke="#303030"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-                ></span
-              >
+        </form>
+        <button
+          type="button"
+          @click="download"
+          class="btn_dl d-none d-md-block"
+        >
+          <span>Download <i class="fa-solid fa-arrow-right"></i></span>
+          <span
+            class="d-none spinner-border spinner-border-sm"
+            aria-hidden="true"
+          ></span>
+        </button>
+        <!-- Download progress bar -->
+        <progress id="downloadProgress" :value="progress" max="100"></progress>
+        <span id="progressText">{{ progress }}% the </span>
+        <button id="cancelButton" @click="cancelDownload" style="display: none">
+          Cancel
+        </button>
 
-              <input
-                type="text"
-                onpaste="setTimeout(() => {download()}, 0);"
-                class="form-control"
-                name="link"
-                v-model="link"
-                required
-                placeholder="Paste your video link here"
-              />
-              <button
-                type="submit"
-                submit="true"
-                class="btn_dl d-none d-md-block"
-              >
-                <span>Download <i class="fa-solid fa-arrow-right"></i></span>
-                <span
-                  class="d-none spinner-border spinner-border-sm"
-                  aria-hidden="true"
-                ></span>
-              </button>
-              <button
-                aria-label="Download"
-                type="submit"
-                submit="true"
-                class="btn_dl p-3 d-block d-md-none"
-                style="border-radius: 20px 45px 45px 20px !important"
-              >
-                <svg
-                  width="24"
-                  height="25"
-                  viewBox="0 0 24 25"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M19 12.076H5M14 17.076l5-5M14 7.076l5 5"
-                    stroke="#fff"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-                <span
-                  class="d-none spinner-border spinner-border-sm"
-                  aria-hidden="true"
-                ></span>
-              </button>
-            </div>
-          </form>
+        <div
+          class="dl_list hidden rounded"
+          id="download_list"
+          style="width: 100%"
+        ></div>
 
-          <div
-            class="dl_list hidden rounded"
-            id="download_list"
-            style="width: 100%"
-          ></div>
-
-          <div class="sup_plt border-top border-1" v-if="Formats">
-            <p>Available Formats:</p>
-            <ul>
-              <div class="wrapper">
-                <div class="option">
-                  <input
-                    class="input"
-                    type="radio"
-                    name="btn"
-                    value="option3"
-                  />
-                  <div class="btn">
-                    <span class="span"></span>
-                  </div>
+        <div class="sup_plt border-top border-1" v-if="Formats">
+          <p>Available Formats:</p>
+          <ul>
+            <div class="wrapper" v-for="Format in Formats" :key="Format">
+              <div class="option">
+                <input
+                  class="input"
+                  v-model="formaVid"
+                  type="radio"
+                  name="btn"
+                  :value="Format.qualityLabel"
+                />
+                <div class="btn">
+                  <span class="span">{{ Format.qualityLabel }}</span>
                 </div>
               </div>
-            </ul>
-          </div>
+            </div>
+          </ul>
+        </div>
 
-          <div
-            v-show="errorMessage"
-            class="sup_plt border-top border-1"
-            style="background-color: red"
-          >
-            <p class="text-center">{{ errorMessage }}</p>
-          </div>
+        <div
+          v-show="errorMessage"
+          class="sup_plt border-top border-1"
+          style="background-color: red; transition: opacity 0.5s ease-in-out"
+        >
+          <p class="text-center">{{ errorMessage }}</p>
         </div>
       </div>
     </div>
   </section>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      progress: 0,
+    };
+  },
+  mounted() {
+    // Fetch video
+    fetch("/download")
+      .then((response) => {
+        const total = Number(response.headers.get("Content-Length"));
+        let loaded = 0;
+
+        // Update progress as data is received
+        const reader = response.body.getReader();
+        const pump = () => {
+          reader.read().then(({ done, value }) => {
+            if (done) return;
+            loaded += value.byteLength;
+            this.progress = Math.min(100, (loaded / total) * 100);
+            pump();
+          });
+        };
+        pump();
+      })
+      .catch((error) => {
+        console.error("Error downloading video:", error.message);
+        // Handle error
+      });
+  },
+};
+</script>
--- server/index.js
+++ server/index.js
@@ -1,26 +1,124 @@
 const express = require("express");
 const cors = require("cors");
 const ytdl = require("ytdl-core");
+const fs = require("fs");
+const SSE = require("sse");
+const http = require("http"); // Import the http module
 
 const app = express();
+const server = http.createServer(app); // Create an HTTP server instance
+
+const sse = new SSE(server); // Pass the server instance to SSE
 
 app.use(express.json()); // Parse JSON request bodies
 app.use(cors()); // Enable CORS for all routes
+
+var formats;
+var videoLink;
+let downloadProgress = 0;
 
 app.get("/", (req, res) => {
   res.status(200).json({ data: "Hello Mohammed" });
 });
 
-app.post("/getData", (req, res) => {
+app.post("/getData", async (req, res) => {
   const link = req.body.link;
-  let videoLink = req.body.link;
+  videoLink = req.body.link;
+  let choiceFormat = req.body.format;
+  console.log("choice", choiceFormat);
+  if (choiceFormat != "") {
+    return res.redirect(
+      "/download?format=" + choiceFormat + "&link=" + videoLink
+    );
+  }
+
   let isValidate = ytdl.validateURL(videoLink);
   if (isValidate) {
-    res.json({ message: "correct video URL" });
-    console.log("yes isValidate");
+    // Get the Video Id using getURLVideoID method
+    let videoID = ytdl.getURLVideoID(videoLink);
+    // Extract the formats from the video ID and send it as a response
+    const { player_response } = await ytdl.getInfo(videoID);
+    formats = player_response.streamingData["formats"];
+    // map the formats to only qualityLabel and quality
+    const formatDetails = formats.map((format) => {
+      return {
+        qualityLabel: format.qualityLabel,
+        contentLength: format.contentLength,
+      };
+    });
+    res.json({ formats: formatDetails });
+    console.log("formats");
   } else {
     res.json({ message: "Error: Invalid video URL" });
   }
+});
+// Array to store connected SSE clients
+const clients = [];
+
+// Function to send download progress to all connected clients
+const sendProgress = (progress) => {
+  clients.forEach((client) => {
+    client.write(`data: ${JSON.stringify({ progress })}\n\n`);
+  });
+};
+
+app.get("/download", async (req, res) => {
+  const format = formats[0]; // Assuming you want to download the first format in the list
+  const videoStream = ytdl(videoLink, { format: format });
+
+  // Set response headers
+  res.setHeader("Content-Type", "video/mp4");
+  res.setHeader("Content-Length", format.contentLength);
+  res.setHeader(
+    "Content-Range",
+    `bytes 0-${format.contentLength - 1}/${format.contentLength}`
+  );
+  res.setHeader("Accept-Ranges", "bytes");
+  res.setHeader("Content-Disposition", `attachment; filename="video.mp4"`);
+
+  // Calculate chunk size for tracking progress
+  const chunkSize = 10 * 1024; // 10 KB
+
+  // Send the video data in chunks
+  videoStream.on("data", (chunk) => {
+    res.write(chunk); // Send the chunk to the client
+    const progress = (res.socket.bytesWritten / format.contentLength) * 100;
+    sendProgress(progress); // Update download progress for SSE clients
+  });
+
+  // Handle errors
+  videoStream.on("error", (error) => {
+    console.error("Error:", error.message);
+    res.status(500).send("Error downloading video");
+  });
+
+  // Handle end of stream
+  videoStream.on("end", () => {
+    console.log("Video downloaded");
+    res.end(); // Finish the response
+  });
+});
+
+// Endpoint to establish SSE connection
+app.get("/progress", (req, res) => {
+  // Set headers for SSE
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  // Send a welcome message to the client
+  res.write("data: Welcome to download progress updates\n\n");
+
+  // Add the client to the list of connected clients
+  clients.push(res);
+
+  // Remove the client from the list when the connection is closed
+  req.on("close", () => {
+    const index = clients.indexOf(res);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+  });
 });
 
 app.listen(3000, () => {
